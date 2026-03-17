904df44d6c71897cb9f722d5d479b3c0567ab580
adriaensen.alexi <adriaensen.alexi@provilion.be>
2026-03-17 14:27:54 +0100

    beetje buggy met 3 en 4

--- rondgande bol.py
+++ rondgande bol.py
@@ -1,23 +1,25 @@
 import pygame
 from pygame.locals import *
+import random
 
 pygame.init()
 pygame.mixer.init()
 
-#screen size
+# screen size
 WIDTH, HEIGHT = 1800, 900
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("BALL")
 clock = pygame.time.Clock()
 
-#1 sircle kleur
-circle_color = (0, 0, 0)
+# 1 cirkel kleur
+circle_color = (255, 0, 100)
 circle_radius = 50
 
 circle_x = WIDTH // 2
 circle_y = HEIGHT // 2
 
-speed = 2
+speed_x = 5
+speed_y = 5
 
 running = True
 while running:
@@ -25,14 +27,17 @@ while running:
         if event.type == QUIT:
             running = False
 
-#2 move sircle
-    circle_x += speed
+    # 2 beweeg cirkel
+    circle_x += speed_x
+    circle_y += speed_y
 
-    # 3 botsen op de rechterkant
-    if circle_x > WIDTH - circle_radius:
-        speed = -speed  # Verander richting bij botsing
+    # 3 botsen op de 4 zijden en willekeurige richting
+    if circle_x > WIDTH - circle_radius or circle_x < circle_radius:
+        speed_x = random.choice([-1, 1]) * abs(speed_x)  # Willekeurige x-richting
+    if circle_y > HEIGHT - circle_radius or circle_y < circle_radius:
+        speed_y = random.choice([-1, 1]) * abs(speed_y)  # Willekeurige y-richting
 
     screen.fill((0, 0, 0))
-    pygame.draw.circle(screen, circle_color, (int(circle_x), circle_y), circle_radius)
+    pygame.draw.circle(screen, circle_color, (int(circle_x), int(circle_y)), circle_radius)
     pygame.display.update()
-    clock.tick(0)
+    clock.tick(60)

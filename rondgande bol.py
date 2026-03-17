3a8a44a11b3dfa9bddc05d27ab55bb9913ab7ad0
adriaensen.alexi <adriaensen.alexi@provilion.be>
2026-03-17 11:51:50 +0100

    constants gemaakt (size)

---
+++ rondgande bol.py
@@ -0,0 +1,17 @@
+import pygame
+import sys
+import random
+
+pygame.init()
+pygame.mixer.init()
+
+WIDTH, HEIGHT = 1800,900
+PADDLE_WIDTH, PADDLE_HEIGHT = 15, 100
+BALL_SIZE = 15
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+FPS = 60
+

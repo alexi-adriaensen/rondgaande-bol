e084f1418c08f24b0a74f54aa63042476d95b779
adriaensen.alexi <adriaensen.alexi@provilion.be>
2026-03-17 13:30:55 +0100

    de bal beweegt

--- rondgande bol.py
+++ rondgande bol.py
@@ -1,21 +1,39 @@
 import pygame
-import sys
-import random
+from pygame.locals import *
 
 pygame.init()
 pygame.mixer.init()
 
-WIDTH, HEIGHT = 1800,900
-PADDLE_WIDTH, PADDLE_HEIGHT = 15, 100
-BALL_SIZE = 15
-
+WIDTH, HEIGHT = 1800, 900
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("BALL")
 clock = pygame.time.Clock()
 
-running = True
+circle_color = (255, 0, 0)
+circle_radius = 50
 
+circle_x = WIDTH // 2
+circle_y = HEIGHT // 2
+
+speed = 5
+
+running = True
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == QUIT:
             running = False
+
+
+    circle_x += speed
+
+
+    if circle_x > WIDTH + circle_radius:
+        circle_x = -circle_radius
+
+
+    screen.fill((0, 0, 0))
+    pygame.draw.circle(screen, circle_color, (circle_x, circle_y), circle_radius)
+
+    pygame.display.update()
+    clock.tick(60)
+
